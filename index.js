c62d47d6b4042c776a18c63357a027d1d9eda995
Higor Santos <higorsantos92@yahoo.com.br>
2022-01-18 20:53:58 -0300

    tratamento de erro

--- index.js
+++ index.js
@@ -31,34 +31,39 @@ app.get("/", (req, res) => {
 app.get("/ativos/tesouro-direto", (req, res) => {
   const ativo = req.query.ativo;
 
-  axios(endpoint).then((response) => {
-    const html = response.data;
+  axios(endpoint)
+    .then((response) => {
+      const html = response.data;
 
-    const $ = cheerio.load(html);
+      const $ = cheerio.load(html);
 
-    $(`div${elTitulos}`, html).each(function () {
-      const nomeAtivo = $(this).find("a.info h4").text().replaceAll("\n", "");
-      const precoAtivo = $(this)
-        .find("a.info div")
-        .slice(3, 4)
-        .find("span")
-        .text()
-        .trim()
-        .replaceAll("\n", "")
-        .replace("vendaR$", "")
-        .replace(".", "")
-        .replace("venda R$", "");
+      $(`div${elTitulos}`, html).each(function () {
+        const nomeAtivo = $(this).find("a.info h4").text().replaceAll("\n", "");
+        const precoAtivo = $(this)
+          .find("a.info div")
+          .slice(3, 4)
+          .find("span")
+          .text()
+          .trim()
+          .replaceAll("\n", "")
+          .replace("vendaR$", "")
+          .replace(".", "")
+          .replace("venda R$", "");
 
-      ativos[nomeAtivo] = precoAtivo;
+        ativos[nomeAtivo] = precoAtivo;
+      });
+
+      const retorno = {};
+      if (ativo) {
+        res.send(ativos[ativo] + "");
+        return;
+      }
+      res.json(ativos);
+    })
+    .catch((erro) => {
+      console.error(erro);
+      res.send(erro);
     });
-
-    const retorno = {};
-    if (ativo) {
-      res.send(ativos[ativo] + "");
-      return;
-    }
-    res.json(ativos);
-  });
 });
 
 app.listen(PORT, () => {
